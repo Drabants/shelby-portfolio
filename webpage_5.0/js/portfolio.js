0faf2bcd093151a2174af2ebf43105841caa84b3
Drabants <drabants@gmail.com>
2017-05-18 15:53:19 -0700

    Add files via upload

--- webpage_5.0/js/portfolio.js
+++ webpage_5.0/js/portfolio.js
@@ -1,14 +1,22 @@
-function cloudMove() {
-  var elem = document.getElementById("binaryBites");   
-  var pos = 0;
-  var id = setInterval(frame, 1);
-  function frame() {
-    if (pos == 1000) {
-      pos = -100;
-    } else {
-      pos++; 
-      elem.style.top = pos - 'px'; 
-      elem.style.left = pos + 'px'; 
+function startClouds(){
+    var cloudArray = new Array();
+    cloudArray.push(document.getElementById("binaryBites"));
+    cloudArray.push(document.getElementById("masterMind"));
+    cloudArray.push(document.getElementById("cipher"));
+    cloudArray[3] = document.getElementById("oldPortfolio");
+    yPos = (Math.random()*500) +1
+    xPos = (Math.random()*1500) +1
+    cloudArray[2].style.top= yPos+"px";
+    cloudArray[2].style.left= xPos+"px";
+    placeClouds(cloudArray);
+};
+
+function placeClouds(cloudArray){
+    for(i=0; i<cloudArray.length; i++){
+    yPos = (Math.random()*500) +1
+    xPos = (Math.random()*1500) +1
+    cloudArray[i].style.top= yPos+"px";
+    cloudArray[i].style.left= xPos+"px";
     }
-  }
-}
+
+};
