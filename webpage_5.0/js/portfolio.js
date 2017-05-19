1bee91dcf7713389ffaa62a34426f1037749f022
Drabants <drabants@gmail.com>
2017-05-19 16:00:13 -0700

    Add files via upload
    random div spawn with set area to aloud cloud bouncing

--- webpage_5.0/js/portfolio.js
+++ webpage_5.0/js/portfolio.js
@@ -3,12 +3,23 @@ function startClouds(){
     cloudArray.push(document.getElementById("binaryBites"));
     cloudArray.push(document.getElementById("masterMind"));
     cloudArray.push(document.getElementById("cipher"));
-    cloudArray[3] = document.getElementById("oldPortfolio");
-    yPos = (Math.random()*500) +1
+    cloudArray.push(document.getElementById("oldPortfolio"));
+    setCloudSpace(cloudArray);
+    for(i=0; i<cloudArray.length; i++){
+        animateMe(cloudArray[i].img, 20);
+    }
+};
+
+function setCloudSpace(cloudArray){
+    for(i=0; i<cloudArray.length; i++){
+    yPos = (Math.random()*300) +1
     xPos = (Math.random()*1500) +1
-    cloudArray[2].style.top= yPos+"px";
-    cloudArray[2].style.left= xPos+"px";
-    placeClouds(cloudArray);
+    cloudArray[i].style.top= yPos+"px";
+    cloudArray[i].style.left= xPos+"px";
+    cloudArray[i].style.width= 500+"px";
+    cloudArray[i].style.height= 300+"px";
+    cloudArray[i].style.border= "1px #CCC solid";
+    }    
 };
 
 function placeClouds(cloudArray){
@@ -19,4 +30,19 @@ function placeClouds(cloudArray){
     cloudArray[i].style.left= xPos+"px";
     }
 
+};
+
+function animateMe(targetElement, speed){
+    $("img").animate(
+        {
+        'left': $(document).width() + 200
+        }, 
+        { 
+        duration: speed, 
+        complete: function(){
+            animateMe(this, speed);
+            }
+        }
+    );
+    
 };
